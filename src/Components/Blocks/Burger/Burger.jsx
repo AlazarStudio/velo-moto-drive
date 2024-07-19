9418cef31d714d3fa62171fd46ee7523fd4a1c2c
MusaJin <mussak949@gmail.com>
2024-07-19 23:05:42 +0300

    bug fix header

--- src/Components/Blocks/Burger/Burger.jsx
+++ src/Components/Blocks/Burger/Burger.jsx
@@ -33,31 +33,21 @@ function Burger() {
 				className={`${styles.nav} ${isOpen ? styles.open : styles.closed}`}
 			>
 				<ul className={styles.nav_list}>
-					<li>
 						<Link to='/' onClick={() => setIsOpen(false)}>
 							<p className={styles.burger_item}>ГЛАВНАЯ</p>
 						</Link>
-					</li>
-					<li>
 						<Link to='/catalog' onClick={() => setIsOpen(false)}>
 							<p className={styles.burger_item}>КАТАЛОГ</p>
 						</Link>
-					</li>
-					<li>
 						<Link to='/delivery' onClick={() => setIsOpen(false)}>
 							<p className={styles.burger_item}>ДОСТАВКА</p>
 						</Link>
-					</li>
-					<li>
 						<Link to='/?section=about_us' onClick={() => setIsOpen(false)}>
 							<p className={styles.burger_item}>О НАС</p>
 						</Link>
-					</li>
-					<li>
 						<Link to='/?section=contacts' onClick={() => setIsOpen(false)}>
 							<p className={styles.burger_item}>КОНТАКТЫ</p>
 						</Link>
-					</li>
 				</ul>
 			</nav>
 		</>
